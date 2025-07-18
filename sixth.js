e4e8633394422972880f6ba436bd4eee297fc1fe
Shrushtigoswami <shrushtigoswami2105@gmail.com>
2025-07-18 10:09:11 +0530

    Print multiplication table

---
+++ sixth.js
@@ -0,0 +1,24 @@
+// let number=5;
+
+// // for(let i=1;i<=10;i++){
+// //     console.log(number+"x"+  i + ' = '+(number*i));
+// //     }
+
+
+//     // let number = 5;
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(number + " x " + i + " = " + (number * i));
+// }
+
+// let n = 5;
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(n + " * " + i + " = " + (n * i));
+// }
+
+let n = 5;
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`${n} * ${i} = ${n * i}`);
+}
